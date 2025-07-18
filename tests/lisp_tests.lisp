a9f810b85f2158da99e147ceb313697e32709426
solve-coagulate <ben.burns@gmail.com>
2025-07-18 12:06:24 +0800

    Expand Lisp interpreter test suite

--- tests/lisp_tests.lisp
+++ tests/lisp_tests.lisp
@@ -6,11 +6,37 @@
 
 (define verbose 1)
 
-(define failures
-  (+ (run-test (quote addition) (= (+ 1 1) 2) verbose)
-     (run-test (quote car) (= (car (list 1 2 3)) 1) verbose)))
+(define length
+  (lambda (xs)
+    (if (null? xs) 0 (+ 1 (length (cdr xs))))) )
 
-(define total 2)
+(define sum
+  (lambda (xs)
+    (if (null? xs) 0 (+ (car xs) (sum (cdr xs))))) )
 
-(print total (quote tests) (quote run) (- total failures) (quote passed) failures (quote failed))
+(define tests
+  (list
+    (run-test (quote addition) (= (+ 1 1) 2) verbose)
+    (run-test (quote car) (= (car (list 1 2 3)) 1) verbose)
+    (run-test (quote cdr) (= (cdr (list 1 2 3)) (list 2 3)) verbose)
+    (run-test (quote cons) (= (cons 1 (list 2 3)) (list 1 2 3)) verbose)
+    (run-test (quote lambda) (= ((lambda (x) (+ x 1)) 2) 3) verbose)
+    (run-test (quote if) (= (if (> 2 1) 42 0) 42) verbose)
+    (run-test (quote null) (null? (cdr (list 1))) verbose)
+    (run-test (quote list?) (list? (list 1 2)) verbose)
+    (run-test (quote quote) (= (quote (1 2 3)) (list 1 2 3)) verbose)
+    (run-test
+      (quote macro)
+      (= (progn
+           (defmacro when (test body)
+             (list (quote if) test body nil))
+           (when (> 2 1) 5))
+         5)
+      verbose)))
+
+(define total (length tests))
+(define failures (sum tests))
+
+(print total (quote tests) (quote run)
+      (- total failures) (quote passed) failures (quote failed))
 (if (= failures 0) 0 1)
