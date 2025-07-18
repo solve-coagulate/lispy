8f2808920ad7b8b6ff9ddd984ecc0671b248891d
solve-coagulate <ben.burns@gmail.com>
2025-07-18 11:47:50 +0800

    Remove set! usage in Lisp tests

--- tests/lisp_tests.lisp
+++ tests/lisp_tests.lisp
@@ -1,21 +1,16 @@
-(define total 0)
-(define failures 0)
-
 (define run-test
   (lambda (name expr verbose)
-    (progn
-      (set! total (+ total 1))
-      (if expr
-          (if verbose (print name (quote PASS)) nil)
-          (progn (print name (quote FAIL))
-                 (set! failures (+ failures 1))
-                 nil))))
-)
+    (if expr
+        (progn (if verbose (print name (quote PASS)) nil) 0)
+        (progn (print name (quote FAIL)) 1))))
 
 (define verbose 1)
 
-(run-test (quote addition) (= (+ 1 1) 2) verbose)
-(run-test (quote car) (= (car (list 1 2 3)) 1) verbose)
+(define failures
+  (+ (run-test (quote addition) (= (+ 1 1) 2) verbose)
+     (run-test (quote car) (= (car (list 1 2 3)) 1) verbose)))
+
+(define total 2)
 
 (print total (quote tests) (quote run) (- total failures) (quote passed) failures (quote failed))
 (if (= failures 0) 0 1)
